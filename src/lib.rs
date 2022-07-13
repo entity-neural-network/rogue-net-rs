1850a84bed69ef7422cf6142256f80eacb2ecab9
Clemens Winter <clemenswinter1@gmail.com>
2022-07-13 06:59:42 -0700

    Test

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,14 @@ mod layer_norm;
 mod linear;
 mod msgpack;
 mod rogue_net;
+#[cfg(test)]
+mod tests;
 mod transformer;
 
 use std::path::Path;
 
+pub use crate::rogue_net::RogueNet;
 use msgpack::decode_state_dict;
-pub use rogue_net::RogueNet;
 
 pub fn load_checkpoint<P: AsRef<Path>>(path: P) -> RogueNet {
     let bytes = std::fs::read(path).unwrap();
